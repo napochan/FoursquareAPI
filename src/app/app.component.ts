d71dcb4f7747094245d5758f0e3882ad35c37933
Weizong Chen <dreamtheatre@WeizongChensMBP.home>
2018-03-10 15:46:13 +0000

    add input html and css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
+import 'style-loader!./app.component.scss';
 
 @Component({
     selector: 'app-shell',
-    template: `Hello World`
+    templateUrl: 'app.component.html'
 })
 
 export class AppComponent {
